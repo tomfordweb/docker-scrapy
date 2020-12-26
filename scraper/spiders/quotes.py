242611da9be43f9897ea2b8ac3a9d20ef7e78503
Tom Ford <tomfordweb@gmail.com>
2020-12-26 09:32:00 -0500

    Scrape data

--- scraper/spiders/quotes.py
+++ scraper/spiders/quotes.py
@@ -1,10 +1,15 @@
 import scrapy
 
-
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = [
+        'http://quotes.toscrape.com',
+    ]
 
     def parse(self, response):
-        pass
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
